5db00ae1d5bceced51661284b28ee732101e7bf6
Abhinav Prasad <39187166+avi9prasad@users.noreply.github.com>
2023-07-06 17:27:19 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,22 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
+function sort(str1)
+{
+var array = str.split(" ");
+array = array.sort();
+var sortedString = array.join("' ");
+return sortedString
 
 }
 
-module.exports = isAnagram;
+function isAnagram(str1, str2) {
+
+  if(sort(str1) == sort(str2)){
+    return true;
+  }
+  else
+  {
+   return false;
+  }
+}
